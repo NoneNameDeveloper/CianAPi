cafd1e31f8517619d0bd2bce580198a2d7d6c980
homy <homycoder@gmail.com>
2023-06-02 16:18:46 +0300

    Added post_date field to Item model

---
+++ cianapi/models/Item.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class Seller(BaseModel):
+	type_: str
+	profile_link: str
+	name: str
+
+
+class Item(BaseModel):
+	title: str
+	link: str
+	adress: str
+	seller: Seller
+	photos: list[str]
+	post_date = datetime
